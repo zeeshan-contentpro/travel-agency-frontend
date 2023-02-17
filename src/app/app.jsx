2e0fdf2f742ec52f5c1b3123664034e2cbf38985
Shafin <shafin.greeho@gmail.com>
2023-02-17 16:22:00 +0600

    next init

--- src/app/app.jsx
+++
@@ -1,5 +0,0 @@
-import "./app.css";
-
-export function App() {
-  return <>hello</>;
-}
--- src/main/main.jsx
+++
@@ -1,5 +0,0 @@
-import { render } from "preact";
-import { App } from "../app/app";
-import "./index.css";
-
-render(<App />, document.getElementById("app"));
