b8ec9585604719b60935e4286fc94557554975ce
shafin003 <100669577+shafin003@users.noreply.github.com>
2023-02-26 01:38:58 +0600

    bug fixes

--- app/(Home)/audioVisual.jsx
+++ app/(Home)/audioVisual.jsx
@@ -3,11 +3,13 @@ import styles from "./audioVisual.module.css";
 export default function AudioVisual() {
     return (
         <div className={styles.main}>
-            <h1>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </h1>
+            <h3>Lorem ipsum dolor</h3>
+            <h1 className={styles.title}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
             <br />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt voluptas illum accusantium!</p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Deserunt voluptas illum accusantium!
+            </p>
         </div>
     );
 }
