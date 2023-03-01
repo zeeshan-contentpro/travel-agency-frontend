5adbe265527eef0424498f0462f99fe288d81720
shafin003 <100669577+shafin003@users.noreply.github.com>
2023-03-01 20:22:24 +0600

    menu bug fixes

--- app/NavFooter/nav.jsx
+++ app/NavFooter/nav.jsx
@@ -3,11 +3,22 @@ import { useState } from "react";
 import styles from "./nav.module.css";
 import { BiMenu } from "react-icons/bi";
 import { IoClose } from "react-icons/io5";
+import Link from "next/link";
 
-function Menu() {
+function Menu({ close }) {
     return (
         <div className={styles.menu}>
-    
+            <div className={styles.left} onClick={() => close()}></div>
+            <div className={styles.right}>
+                <Link href="/" onClick={() => close()}>Home</Link>
+                <hr className={styles.line}/>
+                <Link  href="/" onClick={() => close()}>About us</Link>
+                <hr className={styles.line}/>
+                <Link href="/visa" onClick={() => close()}>Visa info</Link>
+                <hr className={styles.line}/>
+                <Link href="/" onClick={() => close()}>Contact us</Link>
+         
+            </div>
         </div>
     );
 }
@@ -15,20 +26,31 @@ function Menu() {
 export default function Nav() {
     const [viewMenu, setViewMenu] = useState(false);
     return (
-        <div className={styles.main} style={viewMenu ? {backgroundColor: "rgba(0, 0, 0, 0.6)"} : null}>
-            <img className={styles.logo} src="./logo.svg" alt="logo" />
-            {!viewMenu ? (
-                <BiMenu
-                    className={styles.icon}
-                    onClick={() => setViewMenu(true)}
-                />
-            ) : (
-                <IoClose
-                    className={styles.icon}
-                    onClick={() => setViewMenu(false)}
+        <>
+            <div
+                className={styles.main}
+                style={viewMenu ? { background: "rgba(0, 0, 0, 0.9)" } : null}
+            >
+                <img className={styles.logo} src="./logo.svg" alt="logo" />
+                {!viewMenu ? (
+                    <BiMenu
+                        className={styles.icon}
+                        onClick={() => setViewMenu(true)}
+                    />
+                ) : (
+                    <IoClose
+                        className={styles.icon}
+                        onClick={() => setViewMenu(false)}
+                    />
+                )}
+            </div>
+            {viewMenu && (
+                <Menu
+                    close={() => {
+                        setViewMenu(close);
+                    }}
                 />
             )}
-            {viewMenu && <Menu />}
-        </div>
+        </>
     );
 }
