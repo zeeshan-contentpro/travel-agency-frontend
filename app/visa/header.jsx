97a51dc3ad40c5d0ca0a71d3e058f95125b13038
Maruf Hossain <maruf.nsu92@gmail.com>
2023-03-03 00:37:41 +0600

    Visa Centre json file added

--- app/visa/header.jsx
+++ app/visa/header.jsx
@@ -1,9 +1,21 @@
+import { Link } from "react-router-dom";
 import styles from "./header.module.css";
+// import { useEffect, useState } from "react";
+import visaCentre from "../../public/assets/visa_centre.json";
 
 export default function Header() {
   return (
     <div className={styles.main}>
-      
+      <div className="dropdown">
+        <select defaultValue={"DEFAULT"}>
+          <option value="DEFAULT" disabled>
+            --Select Country--
+          </option>
+          {visaCentre.map((result) => (
+            <option key={result.id}>{result.country}</option>
+          ))}
+        </select>
+      </div>
     </div>
   );
 }
