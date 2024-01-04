4c69847667401f321c6588da33471ac16a176428
Maruf Hossain <maruf.nsu92@gmail.com>
2024-01-04 19:51:35 +0600

    Dashboard ui slightly changed

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,86 +1,85 @@
-'use client';
-import React, { useEffect, useState } from 'react';
-import Image from 'next/image';
-import { useSession } from 'next-auth/react';
-import styles from './page.module.css';
-import HotelInfo from './hotelInfo';
-import FlightInfo from './flightInfo';
-import { useRouter } from 'next/navigation';
+"use client";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import styles from "./page.module.css";
+import HotelInfo from "./hotelInfo";
+import FlightInfo from "./flightInfo";
+import { useRouter } from "next/navigation";
+import BookedHotel from "./bookedHotel";
 
 const Dashboard = () => {
-	const { data: session } = useSession();
-	const [hotelData, setHotelData] = useState(null);
-	const [flightData, setFlightData] = useState(null);
-	const router = useRouter();
+  const { data: session } = useSession();
+  const [hotelData, setHotelData] = useState(null);
+  const [flightData, setFlightData] = useState(null);
+  const router = useRouter();
 
-	useEffect(() => {
-		const user = localStorage.getItem('user');
-		if (!user) {
-			router.push('/login');
-		}
-	}, []);
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    if (!user) {
+      router.push("/login");
+    }
+  }, [router]);
 
-	useEffect(() => {
-		// Retrieve data from localStorage
-		const dataFromLocalStorage = localStorage.getItem('hotelInfo');
-		if (dataFromLocalStorage) {
-			const parsedData = JSON.parse(dataFromLocalStorage);
-			setHotelData(parsedData);
-		}
-		const dataFromLocalStorage2 = localStorage.getItem(
-			'flightBookingStatus'
-		);
-		if (dataFromLocalStorage2) {
-			const parsedData = JSON.parse(dataFromLocalStorage2);
-			setFlightData(parsedData);
-		}
-	}, []);
+  useEffect(() => {
+    // Retrieve data from localStorage
+    const dataFromLocalStorage = localStorage.getItem("hotelInfo");
+    if (dataFromLocalStorage) {
+      const parsedData = JSON.parse(dataFromLocalStorage);
+      setHotelData(parsedData);
+    }
+    const dataFromLocalStorage2 = localStorage.getItem("flightBookingStatus");
+    if (dataFromLocalStorage2) {
+      const parsedData = JSON.parse(dataFromLocalStorage2);
+      setFlightData(parsedData);
+    }
+  }, []);
 
-	return (
-		<div className={styles.main}>
-			<h1>Dashboard</h1>
-			<div className={styles.profile}>
-				<div>
-					<Image
-						src={session?.user.image || '/logo.svg'}
-						alt=""
-						height={100}
-						width={100}
-						className={styles.profileImage}
-					/>
-				</div>
-				<div>
-					<h3>Hello {session?.user?.name}</h3>
-					<p>{session?.user?.email}</p>
-					{/* <p>{session?.user?.phone}</p> */}
-				</div>
-			</div>
+  return (
+    <div className={styles.main}>
+      <h1>Dashboard</h1>
+      <div className={styles.profile}>
+        <div>
+          <Image
+            src={session?.user.image || "/logo.svg"}
+            alt=""
+            height={100}
+            width={100}
+            className={styles.profileImage}
+          />
+        </div>
+        <div>
+          <h3>Hello {session?.user?.name}</h3>
+          <p>{session?.user?.email}</p>
+          {/* <p>{session?.user?.phone}</p> */}
+        </div>
+      </div>
 
-			<div className={styles.bookedContainer}>
-				<h2>Your Booked Package Details</h2>
-				<hr />
-				<div className={styles.bookedData}>
-					{hotelData ? (
-						<HotelInfo hotel={hotelData} />
-					) : (
-						<p>No Package booked yet.</p>
-					)}
-				</div>
-			</div>
+      <div className={styles.bookedContainer}>
+        <h2>Your Booked Package Details</h2>
+        <hr />
+        <div className={styles.bookedData}>
+          {hotelData ? (
+            <BookedHotel hotelData={hotelData} />
+          ) : (
+            <p>No Package booked yet.</p>
+          )}
+        </div>
+      </div>
 
-			<div className={styles.bookedContainer}>
-				<h2>Your Booked Flight Details</h2>
-				<hr />
-				<div className={styles.bookedData}>
-					{flightData ? (
-						<FlightInfo flight={flightData} />
-					) : (
-						<p>No Flight booked yet.</p>
-					)}
-				</div>
-			</div>
-		</div>
-	);
+      <div className={styles.bookedContainer}>
+        <h2>Your Booked Flight Details</h2>
+        <hr />
+        <div className={styles.bookedData}>
+          {flightData ? (
+            <FlightInfo flight={flightData} />
+          ) : (
+            <p>No Flight booked yet.</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
